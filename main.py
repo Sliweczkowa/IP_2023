0149d8da75dfbb173bc790b878b08b4a0dc94b8c
wmichalowski <242222@edu.p.lodz.pl>
2023-10-21 14:23:36 +0200

    fix: optimization and processing of color images for contrast

--- main.py
+++ main.py
@@ -17,7 +17,7 @@ def loadImg(path):
 
 # B1 | Image brightness modification
 def brightness(array, num):
-    if array.ndim == 1:
+    if array.ndim == 2:
         for xIndex, x in enumerate(array):
             for yIndex, y in enumerate(x):
                 if y + num >= 255:
@@ -36,23 +36,28 @@ def brightness(array, num):
                         array[xIndex][yIndex][c] = 0
                     else:
                         array[xIndex][yIndex][c] = y[c] + num
-
+    
     return array
 
 
 # B2 | Image contrast modification
 def contrast(array, num):
+
     num /= 2
-    for xIndex, x in enumerate(array):
-        for yIndex, y in enumerate(x):
-            if y - num <= 0:
-                array[xIndex][yIndex] = 0
-            elif y + num >= 255:
-                array[xIndex][yIndex] = 255
-            elif y <= 127:
-                array[xIndex][yIndex] = y - num
-            elif y >= 128:
-                array[xIndex][yIndex] = y + num
+
+    if array.ndim == 2:
+        for xIndex, x in enumerate(array):
+            for yIndex, y in enumerate(x):
+                current_pixel = array[xIndex][yIndex]
+                new_pixel = (current_pixel - 127.5) * num + 127.5
+                array[xIndex][yIndex] = np.clip(new_pixel, 0, 255)
+    elif array.ndim == 3:
+        for xIndex, x in enumerate(array):
+            for yIndex, y in enumerate(x):
+                for c in range(3):
+                    current_pixel = array[xIndex][yIndex][c]
+                    new_pixel = (current_pixel - 127.5) * num + 127.5
+                    array[xIndex][yIndex][c] = np.clip(new_pixel, 0, 255)
     return array
 
 
@@ -253,7 +258,7 @@ imgPath = ""
 
 parser = argparse.ArgumentParser()
 parser.add_argument('--brightness', help='change of brightness', type=int)
-parser.add_argument('--contrast', help='change of contrast', type=int)
+parser.add_argument('--contrast', help='change of contrast', type=float)
 parser.add_argument('--negative', help='negative of the image', action="store_true")
 parser.add_argument('--hflip', help='horizontal flip', action="store_true")
 parser.add_argument('--vflip', help='vertical flip', action="store_true")
