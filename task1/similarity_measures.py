fix: Maximum difference calculation for RGB

task1/similarity_measures.py
@@ -26,7 +26,6 @@ def mse(org_img, noise_img, fil_img):
         sum_org_noise = sqd_dif_sum(org_img, noise_img)
         err_org_noise = sum_org_noise / (width*height)
 
-        return err_org_noise, err_org_fil
 
     if org_img.ndim == 3:
         #original and filtered
@@ -42,7 +41,7 @@ def mse(org_img, noise_img, fil_img):
         err_org_noise = (err_org_noise_r, err_org_noise_g, err_org_noise_b)
         err_org_fil = (err_org_fil_r, err_org_fil_g, err_org_fil_b)
 
-        return err_org_noise, err_org_fil
+    return err_org_noise, err_org_fil
         
 
 
@@ -94,10 +93,27 @@ def psnr(org_img, noise_img, fil_img):
 # E5 | Maximum difference
 def md(org_img, noise_img, fil_img):
 
-    #original and filtered
-    err_org_fil = np.max(np.absolute(org_img - fil_img))
+    if org_img.ndim == 2:
+        #original and filtered
+        err_org_fil = np.max(np.absolute(org_img - fil_img))
 
-    #original and noise
-    err_org_noise = np.max(np.absolute(org_img - noise_img))
+        #original and noise
+        err_org_noise = np.max(np.absolute(org_img - noise_img))
+
+    if org_img.ndim == 3:
+         
+        #original and filtered
+        err_org_fil_r = np.max(np.absolute(org_img[:,:,0] - fil_img[:,:,0]))
+        err_org_fil_g = np.max(np.absolute(org_img[:,:,1] - fil_img[:,:,1]))
+        err_org_fil_b = np.max(np.absolute(org_img[:,:,2] - fil_img[:,:,2]))
+
+        #original and noise
+        err_org_noise_r = np.max(np.absolute(org_img[:,:,0] - noise_img[:,:,0]))
+        err_org_noise_g = np.max(np.absolute(org_img[:,:,1] - noise_img[:,:,1]))
+        err_org_noise_b = np.max(np.absolute(org_img[:,:,2] - noise_img[:,:,2]))
+        
+        err_org_noise = (err_org_noise_r, err_org_noise_g, err_org_noise_b)
+        err_org_fil = (err_org_fil_r, err_org_fil_g, err_org_fil_b)
 
     return err_org_fil, err_org_noise
+    