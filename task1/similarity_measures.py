43d2a099ddcd01914ff2d964fa0fb2ba7083cf18
Sliweczkowa <242231@edu.p.lodz.pl>
2023-11-05 21:28:03 +0100

    restyle: deleted some blank lines, deleted some whitespaces, changed if to elif in mse(), md()

--- task1/similarity_measures.py
+++ task1/similarity_measures.py
@@ -8,8 +8,7 @@ def sqd_dif_sum(img1, img2):
     img2 = img2.astype(np.float32)
 
     sqd_dif = np.square(img1 - img2)
-    sum = np.sum(sqd_dif)
-    return sum
+    return np.sum(sqd_dif)
 
 
 # E1 | Mean square error
@@ -18,7 +17,6 @@ def mse(org_img, noise_img, fil_img):
     width = len(org_img)
 
     if org_img.ndim == 2:
-
         #original and filtered
         sum_org_fil = sqd_dif_sum(org_img, fil_img)
         err_org_fil = sum_org_fil / (width*height)
@@ -27,24 +25,22 @@ def mse(org_img, noise_img, fil_img):
         sum_org_noise = sqd_dif_sum(org_img, noise_img)
         err_org_noise = sum_org_noise / (width*height)
 
-
-    if org_img.ndim == 3:
+    elif org_img.ndim == 3:
         #original and filtered
-        err_org_fil_r = (sqd_dif_sum(org_img[:,:,0], fil_img[:,:,0])) / (width*height)
-        err_org_fil_g = (sqd_dif_sum(org_img[:,:,1], fil_img[:,:,1])) / (width*height)
-        err_org_fil_b = (sqd_dif_sum(org_img[:,:,2], fil_img[:,:,2])) / (width*height)
+        err_org_fil_r = (sqd_dif_sum(org_img[:, :, 0], fil_img[:, :, 0])) / (width*height)
+        err_org_fil_g = (sqd_dif_sum(org_img[:, :, 1], fil_img[:, :, 1])) / (width*height)
+        err_org_fil_b = (sqd_dif_sum(org_img[:, :, 2], fil_img[:, :, 2])) / (width*height)
 
         #original and noise
-        err_org_noise_r = (sqd_dif_sum(org_img[:,:,0], noise_img[:,:,0])) / (width*height)
-        err_org_noise_g = (sqd_dif_sum(org_img[:,:,1], noise_img[:,:,1])) / (width*height)
-        err_org_noise_b = (sqd_dif_sum(org_img[:,:,2], noise_img[:,:,2])) / (width*height)
+        err_org_noise_r = (sqd_dif_sum(org_img[:, :, 0], noise_img[:, :, 0])) / (width*height)
+        err_org_noise_g = (sqd_dif_sum(org_img[:, :, 1], noise_img[:, :, 1])) / (width*height)
+        err_org_noise_b = (sqd_dif_sum(org_img[:, :, 2], noise_img[:, :, 2])) / (width*height)
 
         err_org_noise = (err_org_noise_r, err_org_noise_g, err_org_noise_b)
         err_org_fil = (err_org_fil_r, err_org_fil_g, err_org_fil_b)
 
     return err_org_noise, err_org_fil
         
-
 
 # E2 | Peak mean square error
 def pmse(org_img, noise_img, fil_img):
@@ -108,20 +104,18 @@ def md(org_img, noise_img, fil_img):
         #original and noise
         err_org_noise = np.max(np.absolute(org_img - noise_img))
 
-    if org_img.ndim == 3:
-         
+    elif org_img.ndim == 3:
         #original and filtered
-        err_org_fil_r = np.max(np.absolute(org_img[:,:,0] - fil_img[:,:,0]))
-        err_org_fil_g = np.max(np.absolute(org_img[:,:,1] - fil_img[:,:,1]))
-        err_org_fil_b = np.max(np.absolute(org_img[:,:,2] - fil_img[:,:,2]))
+        err_org_fil_r = np.max(np.absolute(org_img[:, :, 0] - fil_img[:, :, 0]))
+        err_org_fil_g = np.max(np.absolute(org_img[:, :, 1] - fil_img[:, :, 1]))
+        err_org_fil_b = np.max(np.absolute(org_img[:, :, 2] - fil_img[:, :, 2]))
 
         #original and noise
-        err_org_noise_r = np.max(np.absolute(org_img[:,:,0] - noise_img[:,:,0]))
-        err_org_noise_g = np.max(np.absolute(org_img[:,:,1] - noise_img[:,:,1]))
-        err_org_noise_b = np.max(np.absolute(org_img[:,:,2] - noise_img[:,:,2]))
+        err_org_noise_r = np.max(np.absolute(org_img[:, :, 0] - noise_img[:, :, 0]))
+        err_org_noise_g = np.max(np.absolute(org_img[:, :, 1] - noise_img[:, :, 1]))
+        err_org_noise_b = np.max(np.absolute(org_img[:, :, 2] - noise_img[:, :, 2]))
         
         err_org_noise = (err_org_noise_r, err_org_noise_g, err_org_noise_b)
         err_org_fil = (err_org_fil_r, err_org_fil_g, err_org_fil_b)
 
     return err_org_fil, err_org_noise
-    
