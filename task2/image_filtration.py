8587b741b2ffd7dd8c35474c14343e139cb8e599
wmichalowski <242222@edu.p.lodz.pl>
2023-11-16 23:09:04 +0100

    feat: image filtration for RGB

--- task2/image_filtration.py
+++ task2/image_filtration.py
@@ -25,18 +25,20 @@ def sexdetii(arr, mask):
 
     result = np.zeros_like(arr)
 
-    for p in range(border, P-border):
-        for q in range(border, Q-border):
-            neighborhood = arr[p - border: p + border + 1, q - border: q + border + 1]
-            result[p,q] = np.sum(neighborhood * h)
+    if arr.ndim == 2:
+        for p in range(border, P-border):
+            for q in range(border, Q-border):
+                neighborhood = arr[p - border: p + border + 1, q - border: q + border + 1]
+                result[p,q] = abs(np.sum(neighborhood * h))
+    
+    elif arr.ndim == 3:
+        for c in range(3):
+            for p in range(border, P-border):
+                for q in range(border, Q-border):
+                    neighborhood = arr[p - border: p + border + 1, q - border: q + border + 1, c]
+                    result[p,q, c] = abs(np.sum(neighborhood * h))
 
-    for i in range(border):
-        result[i, :] = arr[i, :]
-        result[P - border + i, :] = arr[P - border + i, :]
-        result[:, i] = arr[:, i]
-        result[:, Q - border + i] = arr[:, Q - border + i]
-
-    return result
+    return np.clip(result, 0, 255)
 
 # S4.2
 
@@ -48,16 +50,18 @@ def sexdetii2(arr: np.array):
 
     result = np.zeros_like(arr)
 
-    for p in range(1, P-1):
-        for q in range(1, Q-1):
-            neighborhood = arr[p - 1: p + 2, q - 1: q + 2]
-            result[p,q] = abs(np.sum(neighborhood * h))
-
-    for i in range(1):
-        result[i, :] = arr[i, :]
-        result[P - 1 + i, :] = arr[P - 1 + i, :]
-        result[:, i] = arr[:, i]
-        result[:, Q - 1 + i] = arr[:, Q - 1 + i]
+    if arr.ndim == 2:
+        for p in range(1, P-1):
+            for q in range(1, Q-1):
+                neighborhood = arr[p - 1: p + 2, q - 1: q + 2]
+                result[p,q] = abs(np.sum(neighborhood * h))
+    
+    elif arr.ndim == 3:
+        for c in range(3):
+            for p in range(1, P-1):
+                for q in range(1, Q-1):
+                    neighborhood = arr[p - 1: p + 2, q - 1: q + 2, c]
+                    result[p,q, c] = abs(np.sum(neighborhood * h))
 
     return np.clip(result, 0, 255)
 
@@ -71,17 +75,22 @@ def osobel(arr):
 
     result = np.zeros_like(arr)
 
-    for p in range(border, P-border):
-        for q in range(border, Q-border):
-            neighborhood = arr[p - border: p + border + 1, q - border: q + border + 1]
-            X = (neighborhood[0, 2] + 2 * neighborhood[1, 2] + neighborhood[2,2]) - (neighborhood[0,0] + 2 * neighborhood[1,0] + neighborhood[2,0])
-            Y = (neighborhood[0,0] + 2 * neighborhood[0,1]+ neighborhood[0,2]) - (neighborhood[2,0] + 2 * neighborhood[2,1] + neighborhood[2,2])
-            result[p,q] = abs(np.sqrt(np.square(X)+np.square(Y)))
 
-    for i in range(border):
-        result[i, :] = arr[i, :]
-        result[P - border + i, :] = arr[P - border + i, :]
-        result[:, i] = arr[:, i]
-        result[:, Q - border + i] = arr[:, Q - border + i]
+    if arr.ndim == 2:
+        for p in range(border, P-border):
+            for q in range(border, Q-border):
+                neighborhood = arr[p - border: p + border + 1, q - border: q + border + 1]
+                X = (neighborhood[0, 2] + 2 * neighborhood[1, 2] + neighborhood[2,2]) - (neighborhood[0,0] + 2 * neighborhood[1,0] + neighborhood[2,0])
+                Y = (neighborhood[0,0] + 2 * neighborhood[0,1]+ neighborhood[0,2]) - (neighborhood[2,0] + 2 * neighborhood[2,1] + neighborhood[2,2])
+                result[p,q] = abs(np.sqrt(np.square(X)+np.square(Y)))
+    
+    elif arr.ndim == 3:
+        for c in range(3):
+            for p in range(border, P-border):
+                for q in range(border, Q-border):
+                    neighborhood = arr[p - border: p + border + 1, q - border: q + border + 1, c]
+                    X = (neighborhood[0, 2] + 2 * neighborhood[1, 2] + neighborhood[2,2]) - (neighborhood[0,0] + 2 * neighborhood[1,0] + neighborhood[2,0])
+                    Y = (neighborhood[0,0] + 2 * neighborhood[0,1]+ neighborhood[0,2]) - (neighborhood[2,0] + 2 * neighborhood[2,1] + neighborhood[2,2])
+                    result[p,q, c] = abs(np.sqrt(np.square(X)+np.square(Y)))        
 
-    return result
+    return np.clip(result, 0, 255)
