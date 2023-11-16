bcc540b01f0c0e0ee24e355a9f0d903abce7db3b
wmichalowski <242222@edu.p.lodz.pl>
2023-11-16 22:49:36 +0100

    feat: histogram improvement with RGB

--- task2/histogram.py
+++ task2/histogram.py
@@ -29,12 +29,26 @@ def histogram_f(array, channel):
 def huniform(arr, g_min, g_max):
     width = len(arr[0])
     height = len(arr)
-
-    histogram, bins = np.histogram(arr.ravel(), bins=256, range=(0, 255))
+    
     new_arr = np.zeros_like(arr)
 
-    for i in range(height):
-        for j in range(width):
-            new_arr[i, j] = g_min + (g_max-g_min) * (1/(height*width) * np.cumsum(histogram)[arr[i, j]])
+    if arr.ndim == 2:
+        histogram, bins = np.histogram(arr.ravel(), bins=256, range=(0, 255))
+
+        for i in range(height):
+            for j in range(width):
+                new_arr[i, j] = g_min + (g_max-g_min) * (1/(height*width) * np.cumsum(histogram)[arr[i, j]])
+
+    elif arr.ndim == 3:
+        histogram_r, bins = np.histogram(arr[:,:,0].ravel(), bins=256, range=(0, 255))
+        histogram_g, bins = np.histogram(arr[:,:,1].ravel(), bins=256, range=(0, 255))
+        histogram_b, bins = np.histogram(arr[:,:,2].ravel(), bins=256, range=(0, 255))
+
+        histogram = (histogram_r, histogram_g, histogram_b)
+
+        for c in range(3):
+            for x in range(width):
+                for y in range(height):
+                    new_arr[x, y, c] = g_min + (g_max-g_min) * (1/(height*width) * np.cumsum(histogram[c])[arr[x, y, c]])
 
     return new_arr
