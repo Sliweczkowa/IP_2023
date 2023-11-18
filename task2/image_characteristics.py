6fffd31fa5cf8678b9a7fbfbffb4bcc84fde5686
wmichalowski <242222@edu.p.lodz.pl>
2023-11-18 15:51:42 +0100

    feat: image characteristics for rgb

--- task2/image_characteristics.py
+++ task2/image_characteristics.py
@@ -6,14 +6,23 @@ import numpy as np
 
 # C1.1 | Mean
 def cmean(arr, levels):
-    width = len(arr[0])
-    height = len(arr)
+    width = arr.shape[1]
+    height = arr.shape[0]
 
-    histogram, bins = np.histogram(arr.ravel(), bins=levels, range=(0, levels-1))
-    to_sum = 0
+    if arr.ndim == 2:
+        histogram, bins = np.histogram(arr.ravel(), bins=levels, range=(0, levels-1))
+        to_sum = np.sum(np.arange(levels) * histogram)
+        b = [1 / (width * height) * to_sum]
+    
+    elif arr.ndim == 3:
+        b = []
+        for channel in range(arr.shape[2]):
+            channel_data = arr[:, :, channel]
+            histogram, bins = np.histogram(channel_data.ravel(), bins=levels, range=(0, levels-1))
+            to_sum = np.sum(np.arange(levels) * histogram)
+            mean_channel = 1 / (width * height) * to_sum
+            b.append(mean_channel)
 
-    to_sum = np.sum(np.arange(levels) * histogram)
-    b = 1 / (width * height) * to_sum
     return b
 
 
@@ -22,11 +31,22 @@ def cvariance(arr, levels):
     width = len(arr[0])
     height = len(arr)
 
-    histogram, bins = np.histogram(arr.ravel(), bins=levels, range=(0, levels-1))
-    b = cmean(arr, levels)
-    to_sum = 0
-    to_sum = np.sum(np.square(np.arange(levels)-b) * histogram)
-    variance = 1 / (width * height) * to_sum
+    if arr.ndim == 2:
+        histogram, bins = np.histogram(arr.ravel(), bins=levels, range=(0, levels-1))
+        b = cmean(arr, levels)
+        to_sum = np.sum(np.square(np.arange(levels) - b) * histogram)
+        variance = 1 / (width * height) * to_sum
+    
+    elif arr.ndim == 3:
+        variance = []
+        for channel in range(arr.shape[2]):
+            channel_data = arr[:, :, channel]
+            histogram, bins = np.histogram(channel_data.ravel(), bins=levels, range=(0, levels-1))
+            b_channel = cmean(channel_data, levels)
+            to_sum_channel = np.sum(np.square(np.arange(levels) - b_channel) * histogram)
+            variance_channel = 1 / (width * height) * to_sum_channel
+            variance.append(variance_channel)
+
     return variance
 
 
@@ -47,14 +67,25 @@ def cvarcoi(arr, levels):
 
 # C3 | Asymmetry coefficient
 def casyco(arr, levels):
-    width = len(arr[0])
-    height = len(arr)
+    width = arr.shape[1]
+    height = arr.shape[0]
 
-    histogram, bins = np.histogram(arr.ravel(), bins=levels, range=(0, levels-1))
+    if arr.ndim == 2: 
+        s = cstdev(arr, levels)
+        b = cmean(arr, levels)
 
-    s = cstdev(arr, levels)
-    b = cmean(arr, levels)
-    b_s = (1 / np.power(s, 3)) * 1 / width * height * np.sum(np.power((np.arange(levels)-b), 3) * histogram)
+        histogram, bins = np.histogram(arr.ravel(), bins=levels, range=(0, levels-1))
+        b_s = (1 / np.power(s, 3)) * (1 / (width * height)) * np.sum(np.power((np.arange(levels) - b), 3) * histogram)
+    
+    elif arr.ndim == 3: 
+        b_s = np.zeros(arr.shape[2])
+        for channel in range(arr.shape[2]):
+            channel_data = arr[:, :, channel]
+            histogram, bins = np.histogram(channel_data.ravel(), bins=levels, range=(0, levels-1))
+            s_channel = cstdev(channel_data, levels)
+            b_channel = cmean(channel_data, levels)
+            b_s[channel] = (1 / np.power(s_channel, 3)) * ((1 / (width * height)) * np.sum(np.power((np.arange(levels) - b_channel), 3) * histogram))
+
     return b_s
 
 
@@ -63,11 +94,24 @@ def cflaco(arr, levels):
     width = len(arr[0])
     height = len(arr)
 
-    histogram, bins = np.histogram(arr.ravel(), bins=levels, range=(0, levels-1))
 
-    s = cstdev(arr, levels)
-    b = cmean(arr, levels)
-    b_k = (1 / np.power(s, 4)) * 1 / width * height * np.sum(np.power((np.arange(levels)-b), 4) * histogram - 3)
+    if arr.ndim == 2:
+        s = cstdev(arr, levels)
+        b = cmean(arr, levels)
+        
+        histogram, bins = np.histogram(arr.ravel(), bins=levels, range=(0, levels-1))
+        b_k = (1 / np.power(s, 4)) * (1 / (width * height)) * np.sum(np.power((np.arange(levels) - b), 4) * histogram - 3)
+    
+    elif arr.ndim == 3: 
+        b_k = np.zeros(arr.shape[2])
+        for channel in range(arr.shape[2]):
+            channel_data = arr[:, :, channel]
+            histogram, bins = np.histogram(channel_data.ravel(), bins=levels, range=(0, levels-1))
+            s_channel = cstdev(channel_data, levels)
+            b_channel = cmean(channel_data, levels)
+            
+            b_k[channel] = (1 / np.power(s_channel, 4)) * (1 / (width * height)) * np.sum(np.power((np.arange(levels) - b_channel), 4) * histogram - 3)
+    
     return b_k
 
 
@@ -76,9 +120,17 @@ def cvarcoii(arr, levels):
     width = len(arr[0])
     height = len(arr)
 
-    histogram, bins = np.histogram(arr.ravel(), bins=levels, range=(0, levels-1))
+    if arr.ndim == 2:
+        histogram, bins = np.histogram(arr.ravel(), bins=levels, range=(0, levels-1))
 
-    b_n = np.square(1 / (width * height)) * np.sum(np.square(histogram))
+        b_n = np.square(1 / (width * height)) * np.sum(np.square(histogram))
+
+    if arr.ndim == 3: 
+        b_n = np.zeros(arr.shape[2])
+        for c in range(3):
+            channnel_data=arr[:,:,c]
+            histogram, bins = np.histogram(channnel_data.ravel(), bins=levels, range=(0, levels-1))
+            b_n[c] = np.square(1 / (width * height)) * np.sum(np.square(histogram))
     return b_n
 
 
@@ -87,9 +139,19 @@ def centropy(arr, levels):
     width = len(arr[0])
     height = len(arr)
 
-    histogram, bins = np.histogram(arr.ravel(), bins=levels, range=(0, levels-1))
-    epsilon = 1e-10
-    histogram = histogram + epsilon
-   
-    b_e = -1 / (width * height) * np.sum(histogram * np.log2(histogram / (width * height)))
+    if arr.ndim == 2:
+        histogram, bins = np.histogram(arr.ravel(), bins=levels, range=(0, levels-1))
+        epsilon = 1e-10
+        histogram = histogram + epsilon
+        b_e = -1 / (width * height) * np.sum(histogram * np.log2(histogram / (width * height)))
+
+    elif arr.ndim == 3:
+        b_e = np.zeros(arr.shape[2])
+        for c in range(3):
+            channel_data = arr[:,:,c]
+            epsilon = 1e-10
+            histogram, bins = np.histogram(channel_data.ravel(), bins=levels, range=(0, levels-1))
+            histogram = histogram + epsilon
+            b_e[c] = -1 / (width * height) * np.sum(histogram * np.log2(histogram / (width * height)))
+
     return b_e
