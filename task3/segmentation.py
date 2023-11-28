cf96b7289dcaa689b5fbfde2e35bd4ca75bb3238
Sliweczkowa <242231@edu.p.lodz.pl>
2023-11-28 12:51:42 +0100

    feat: regionGrowing() now works for seed pixel list

--- task3/segmentation.py
+++ task3/segmentation.py
@@ -2,29 +2,31 @@ import numpy as np
 
 
 # R1 | Region growing (merging)
-def regionGrowing(seedPoint: (int, int), arrayImage: np.ndarray) -> np.ndarray:
-    region = np.zeros_like(arrayImage)
+def regionGrowing(seedPointList: list[(int, int)], arrayImage: np.ndarray) -> list[np.ndarray]:
+    region = [[], [], []]
+    for i in range(len(seedPointList)):
+        region[i] = np.zeros_like(arrayImage)
 
-    visited = np.zeros_like(arrayImage)
+    for i, seedPoint in enumerate(seedPointList):
+        visited = np.zeros_like(arrayImage)
+        stack = [seedPoint]
 
-    stack = [seedPoint]
+        while stack:
+            currentPoint = stack.pop()
 
-    while stack:
-        currentPoint = stack.pop()
+            if not visited[currentPoint]:
+                if arrayImage[currentPoint] == arrayImage[seedPoint]:
+                    region[i][currentPoint] = 255
 
-        if not visited[currentPoint]:
-            if arrayImage[currentPoint] == arrayImage[seedPoint]:
-                region[currentPoint] = 255
+                    if not currentPoint[1] + 1 == len(arrayImage[0]):
+                        stack.append((currentPoint[0], currentPoint[1] + 1))
+                    if not currentPoint[1] - 1 == 0:
+                        stack.append((currentPoint[0], currentPoint[1] - 1))
+                    if not currentPoint[0] + 1 == len(arrayImage[0]):
+                        stack.append((currentPoint[0] + 1, currentPoint[1]))
+                    if not currentPoint[0] - 1 == 0:
+                        stack.append((currentPoint[0] - 1, currentPoint[1]))
 
-                if not currentPoint[1] + 1 == len(arrayImage[0]):
-                    stack.append((currentPoint[0], currentPoint[1] + 1))
-                if not currentPoint[1] - 1 == 0:
-                    stack.append((currentPoint[0], currentPoint[1] - 1))
-                if not currentPoint[0] + 1 == len(arrayImage[0]):
-                    stack.append((currentPoint[0] + 1, currentPoint[1]))
-                if not currentPoint[0] - 1 == 0:
-                    stack.append((currentPoint[0] - 1, currentPoint[1]))
-
-            visited[currentPoint] = 1
+                visited[currentPoint] = 1
 
     return region
