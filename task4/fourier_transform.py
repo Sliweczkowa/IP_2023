3c35aa2eced611c905c37d1c3e15792f7571c681
wmichalowski <242222@edu.p.lodz.pl>
2023-12-08 22:45:39 +0100

    feat: inverse fft for 2d arrays

--- task4/fourier_transform.py
+++ task4/fourier_transform.py
@@ -40,12 +40,27 @@ def fft(x: np.complex128):
                        even + T[N//2:] * odd])
 
 def fft2d(x: np.complex128):
-    # Apply 1D FFT along the first dimension (rows)
     x_row_fft = np.array([fft(row) for row in x], dtype=np.complex128)
 
-    # Apply 1D FFT along the second dimension (columns)
     x_col_fft = np.array([fft(col) for col in x_row_fft.T], dtype=np.complex128).T
 
     res_arrs = np.fft.fftshift(x_col_fft)
     lres_arrs = 10 * np.log(1+np.abs(res_arrs))
     return lres_arrs, x_col_fft
+
+def ifft(x, final=True):
+  N = len(x)
+  if N <= 1:
+      return x
+  even = ifft(x[0::2], final=False)
+  odd = ifft(x[1::2], final=False)
+  T = np.exp(2j * np.pi * np.arange(N) / N)
+  result = np.concatenate([even + T[:N//2] * odd,
+                          even + T[N//2:] * odd])
+  return result / N if final else result
+
+def ifft2d(x):
+   x_row_ifft = np.array([ifft(row) for row in x], dtype=np.complex128)
+   x_col_ifft = np.array([ifft(col) for col in x_row_ifft.T], dtype=np.complex128).T
+
+   return x_col_ifft
