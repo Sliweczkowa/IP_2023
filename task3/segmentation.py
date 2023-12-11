b2bddb78b95a48ba53acce23566647157539a494
Sliweczkowa <242231@edu.p.lodz.pl>
2023-12-11 12:51:35 +0100

    feat: task3 - region growing for greyscale

--- task3/segmentation.py
+++ task3/segmentation.py
@@ -1,22 +1,25 @@
 import numpy as np
 
 
+# TODO: region growing for rgb, repeating regions deletion for rgb, conditionValue to CLI
+
 # R1 | Region growing (merging)
-def regionGrowing(seedPointList: list[(int, int)], arrayImage: np.ndarray) -> list[np.ndarray]:
+def regionGrowingForOneChannel(seedPointList: list[(int, int)], arrayImage: np.ndarray, conditionValue: int) -> list[np.ndarray]:
     region = []
     for i in range(len(seedPointList)):
         region.append(np.zeros_like(arrayImage))
 
     for i, seedPoint in enumerate(seedPointList):
-        visited = np.zeros_like(arrayImage)
+        visited = np.zeros((len(arrayImage), len(arrayImage[0])), dtype=bool)
         stack = [seedPoint]
 
         while stack:
             currentPoint = stack.pop()
 
             if not visited[currentPoint]:
-                if arrayImage[currentPoint] == arrayImage[seedPoint]:
-                    region[i][currentPoint] = 255
+                if (arrayImage[currentPoint] >= arrayImage[seedPoint] - conditionValue and
+                        arrayImage[currentPoint] <= arrayImage[seedPoint] + conditionValue):
+                    region[i][currentPoint] = arrayImage[currentPoint]
 
                     if not currentPoint[1] + 1 == len(arrayImage[0]):
                         stack.append((currentPoint[0], currentPoint[1] + 1))
@@ -36,4 +39,10 @@ def regionGrowing(seedPointList: list[(int, int)], arrayImage: np.ndarray) -> li
                 region = np.delete(region, j, axis=0)
             j += 1
 
-    return region[0]
+    return region
+
+
+def regionGrowing(seedPointList: list[(int, int)], arrayImage: np.ndarray) -> list[np.ndarray]:
+
+    if arrayImage.ndim == 2:
+        return regionGrowingForOneChannel(seedPointList, arrayImage)[0]
